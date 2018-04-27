d74df6759a47eee360706f2e39820172a5bab43d
axelvestberg <axel.vestberg@gmail.com>
2018-04-27 13:59:57 +0200

    la till statiska funktioner på kartan för att testa

--- imports/ui/components/map.js
+++ imports/ui/components/map.js
@@ -1,17 +1,18 @@
 import '../components/map.html';
 
-//axels maps js api key
-GoogleMaps.load({ v: '3', key: 'AIzaSyAgjN9v8r4q8CBgGXiVnbcqUJASk9KkF3I', libraries: 'geometry,places' });
-
 Meteor.startup(function() {  
-    GoogleMaps.load();
+    GoogleMaps.load({ v: '3', key: 'AIzaSyAgjN9v8r4q8CBgGXiVnbcqUJASk9KkF3I', libraries: 'geometry,places' });
   });
+  
+
+  //let LatLng = new google.maps.LatLng(59.33, 18.07) 
   
   Template.map.helpers({  
     mapOptions: function() {
       if (GoogleMaps.loaded()) {
         return {
             //koordinaterna som visas när kartan laddas
+
           center: new google.maps.LatLng(59.33, 18.07),
           zoom: 11
         };
@@ -20,15 +21,24 @@ Meteor.startup(function() {
   });
 
   //testkod för att se att kartan skapats i consolen
-  Template.map.onCreated(function() {  
+  Template.map.onRendered(function() {  
     GoogleMaps.ready('map', function(map) {
       console.log('Map is ready')
   
       // The code shown below goes here
-     
+      let marker = new google.maps.Marker({
+        position: new google.maps.LatLng(59.338408, 18.054466),
+        
+       map: map.instance
+    })
+   let infowindow = new google.maps.InfoWindow({
+       content: 'Parkinformation: Tegnerlunden'
+   })
+   marker.addListener('click', function(event){
+         infowindow.open(map.instance, marker);
+   })
+
 
     });
   });
-  
-  Markers = new Mongo.Collection('markers');
 
