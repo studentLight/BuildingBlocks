b0f14d2e1ee86a54f6e6cad8e7ad3f59d31bb4c0
Naimie <Kristinaelmgren@gmail.com>
2018-04-29 14:05:38 +0200

    trying to fix a db for challenges w/o much luck

--- imports/ui/pages/challengesDisplayPage.js
+++ imports/ui/pages/challengesDisplayPage.js
@@ -1,3 +1,11 @@
 import './challengesDisplayPage.html';
 
 import '../components/navbar.js';
+
+// import { Challenges } from '../imports/api/challenges.js';
+
+Template.challengesDisplayPage.helpers({
+  challengehelper() {
+    return Challenges.find({});
+  },
+});
