3b3cc82fdd88d16ca50c81dec33c46b4c96ffca0
Naimie <Kristinaelmgren@gmail.com>
2018-05-02 23:17:52 +0200

    messing around with modals still

---
+++ imports/ui/components/modals/modal.js
@@ -0,0 +1,23 @@
+// import './modal.html';
+//
+// function pageInit() {
+//   $(document).ready(function() {
+//     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+//     /*$('.view').click(function (){
+//     	$('#modal1').modal('open');
+//     	alert('edskjcxnm');
+//     });*/
+//     /*$('.view').leanModal();*/
+//     $('#modal1').modal();
+//
+// });
+// }
+//
+// Template.modal.helpers({
+//
+//   'click  #modal1': function() {
+//           BlazeLayout.render('modal');
+//           console.log("button was clicked");
+//       }
+//
+// });
--- imports/ui/pages/challengesDisplayPage.js
+++ imports/ui/pages/challengesDisplayPage.js
@@ -1,5 +1,5 @@
 import './challengesDisplayPage.html';
-import '../components/modal.js';
+// import '../components/modals/modal.js';
 import '../components/navbar.js';
 
 import { Challenges } from '../../api/collections/challenges.js';
@@ -13,10 +13,33 @@ Template.challengesDisplayPage.helpers({
 
 
 Template.challengesDisplayPage.events({
-  'click #modal1': function(e) {
-      e.preventDefault();
-
-      $('#modal1').modal('show');
-      // BlazeLayout.render('#modal');
-    }
+  // 'click #modal1': function(e) {
+  //     e.preventDefault();
+  //
+  //     $('#modal1').modal('show');
+  //     // BlazeLayout.render('#modal');
+  //   }
+  'click  #modal1': function() {
+           $('#modal1').leanModal();
+           // var instance = M.Modal.getInstance(elem);
+           // instace.open();
+        //  BlazeLayout.render('modal');
+          // console.log("button was clicked");
+      }
 });
+//
+// Template.challengesDisplayPage.onRendered({
+// //   $(document).ready(function() {
+// //   $('#modal1').leanModal();
+// // });
+//
+// });
+function pageInit() {
+ $('.modal-trigger').leanModal();
+ console.log("button was clicked");
+}
+
+Template.challengesDisplayPage.rendered = function(){
+    pageInit();
+
+};
