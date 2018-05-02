469410756040d4964329d4064da3640cbcd42308
axelvestberg <axel.vestberg@gmail.com>
2018-05-02 12:48:31 +0200

    more work on infowindows and trying to implement open data from stockholms stad

--- imports/ui/components/map.js
+++ imports/ui/components/map.js
@@ -1,7 +1,7 @@
 import '../components/map.html';
 
 Meteor.startup(function() {  
-    GoogleMaps.load({ v: '3', key: 'AIzaSyAgjN9v8r4q8CBgGXiVnbcqUJASk9KkF3I', libraries: 'geometry,places' });
+    GoogleMaps.load({ v: '3.exp', key: 'AIzaSyAgjN9v8r4q8CBgGXiVnbcqUJASk9KkF3I', libraries: 'geometry,places' });
   });
   
 
@@ -31,8 +31,20 @@ Meteor.startup(function() {
         
        map: map.instance
     })
-   let infowindow = new google.maps.InfoWindow({
-       content: 'Parkinformation: Tegnerlunden'
+
+    let contentstring = '<div id="content" style="text-align: center">'+
+    '<div id="siteNotice">'+
+    '</div>'+
+    '<h4 id="firstHeading" class="firstHeading">Tegnerlunden</h1>'+
+    '<div id="bodyContent">'+
+    '<p><b>Tegnérlunden</b> är en park i centrala Stockholm, på gränsen mellan stadsdelarna Norrmalm och Vasastaden' + 
+    '<a href="parkPage" class="waves-effect waves-light btn">Börja koda</a>'
+    '</div>'+
+    '</div>';
+
+    let infowindow = new google.maps.InfoWindow({
+       content: contentstring,
+       maxWidth: 250,
    })
    marker.addListener('click', function(event){
          infowindow.open(map.instance, marker);
@@ -42,3 +54,10 @@ Meteor.startup(function() {
     });
   });
 
+  const sthlmapi = 'http://api.stockholm.se/ServiceGuideService/ServiceUnitTypes/9da341e4-bdc6-4b51-9563-e65ddc2f7434/ServiceUnits/json?apikey=83cc8184e26f48369d22259c7c016825';
+   
+  fetch(sthlmapi)
+      .then(res => res.json())
+      .then((data) => {
+          console.log('Data: ', data);
+  }).catch(err => console.error(err));
