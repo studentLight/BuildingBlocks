1ea0eae390e2315b00fa0f24e31f588bda977b5a
marcuspousette <marcuspousette@gmail.com>
2018-05-03 15:44:50 +0200

    navbar removed

--- imports/ui/pages/login.js
+++ imports/ui/pages/login.js
@@ -1,3 +1,9 @@
 import './login.html';
 import '../components/header.js';
 import '../components/overrides/overrides.js';
+
+import {pageInitialize} from '../../api/pageInit.js'
+
+Template.parkPage.rendered = function(){
+  pageInitialize();
+};
