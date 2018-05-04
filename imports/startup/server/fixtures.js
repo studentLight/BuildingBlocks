df9246557707589d33f7da7eadd7327db42958a0
Naimie <Kristinaelmgren@gmail.com>
2018-05-04 15:27:21 +0200

    populating the challenge db at startup

--- imports/startup/server/fixtures.js
+++ imports/startup/server/fixtures.js
@@ -1,4 +1,14 @@
 import { Mongo } from 'meteor/mongo';
 
-import '../../api/collections/challenges.js';
+
 import '../../api/collections/lightPosts.js';
+import { Challenges } from '../../api/collections/challenges.js';
+
+Meteor.startup(() => {
+  Challenges.remove({});
+  Challenges.insert({ text: "Utmaning 1", createdAt: new Date()} );
+  Challenges.insert({ text: "Utmaning 2", createdAt: new Date()} );
+  Challenges.insert({ text: "Utmaning 3", createdAt: new Date()} );
+  Challenges.insert({ text: "Utmaning 4", createdAt: new Date()} );
+
+});
