8eba2785006b50c48c1d2fbd0ccb135ed4beeafa
Your Name <bengtj.kjellberg@gmail.com>
2018-05-15 00:20:46 +0200

    Update /B

--- imports/ui/components/blocks/endBlock.js
+++ imports/ui/components/blocks/endBlock.js
@@ -3,4 +3,6 @@ import './endBlock.html';
 
 
 // skriva funktion, som avslutar körningen av sammankopplade kod-block
-// genererar resultat av alla dessa 
+// där efter genereras resultat av alla dessa
+// tar in de värden som anges via dropdowns etc
+// funtkioner för start, if, then, end
--- imports/ui/components/gameButtons.js
+++ imports/ui/components/gameButtons.js
@@ -1,9 +1,12 @@
 import './gameButtons.html';
-
+import '../components/blocks/startBlock.html';
+import '../components/blocks/endBlock.html';
+import '../components/blocks/ifBlock.html';
+import '../components/blocks/thenBlock.html';
 
 
 // skriva händelsefunktion för varje enskild button
-//använda ID't för att skriva lyssnar-funktioner 
-// funtkioner för start, if, then, end
+// använda ID't för att skriva lyssnar-funktioner
+
 // ska generera respektive block bild på gameboard
 // med textuell beskrivning dropdowns, etc
