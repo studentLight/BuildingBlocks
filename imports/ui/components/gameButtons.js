c6da02a4f10e930c3b205d19ec4b08aa7f9eccbb
Albinwal <albin.walheim@hotmail.com>
2018-05-17 12:30:41 +0200

    lägger till nya pusselbitarna

--- imports/ui/components/gameButtons.js
+++ imports/ui/components/gameButtons.js
@@ -9,24 +9,32 @@ Template.gameButtons.events({
 
   "click #startButton": function(event){
       console.log("in startButton klick");
-      var div = createBuildningBlock("images/StartBlock.png");
+      var div = createBuildningBlock("images/startBlock.png");
+      var textDiv = createTextDiv("start-block");
+      div.appendChild(textDiv);
       document.getElementById("placeBlock").appendChild(div);
   },
 
   "click #endButton": function(event){
       console.log("in endButton klick");
-      var div = createBuildningBlock("images/SlutBlock.png");
+      var div = createBuildningBlock("images/endBlock.png");
+      var textDiv = createTextDiv("Stopp-block");
+      div.appendChild(textDiv);
       document.getElementById("placeBlock").appendChild(div);
   },
 
   "click #ifButton": function(event){
       console.log("in ifButton klick");
-      var div = createBuildningBlock("images/OmBlock.png");
+      var div = createBuildningBlock("images/ifBlock.png");
+      var textDiv = createTextDiv("Om-block");
+      div.appendChild(textDiv);
       document.getElementById("placeBlock").appendChild(div);
   },
   "click #thenButton": function(event){
       console.log("in thenButton klick");
-      var div = createBuildningBlock("images/SåBlock.png");
+      var div = createBuildningBlock("images/thenBlock.png");
+      var textDiv = createTextDiv("Så-block");
+      div.appendChild(textDiv);
       document.getElementById("placeBlock").appendChild(div);
   }
 
@@ -46,3 +54,30 @@ function createBuildningBlock(src){
   div.appendChild(img);
   return div;
 }
+function createTextDiv(text){
+  var content = document.createElement("div");
+  content.style.position = "absolute";
+  content.style.bottom = "40%";
+  content.style.right = "40%";
+
+  var paragraph = document.createElement("P");
+  paragraph.innerText = text;
+
+
+  content.appendChild(paragraph);
+  return content;
+
+}
+/*function createActionDiv(text){
+  var content = document.createElement("div");
+  content.style.textAlign = "center";
+  content.style.top = "50%";
+
+  var paragraph = document.createElement("P");
+  paragraph.innerText = text;
+
+
+  content.appendChild(paragraph);
+  return content;
+
+}*/
