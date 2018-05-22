a4fde60de6e13c44707d5794ef4d1a03d674e4a4
marcuspousette <marcuspousette@gmail.com>
2018-05-22 18:43:44 +0200

    fixing a form for the activation of sensors

--- imports/ui/pages/homePage.js
+++ imports/ui/pages/homePage.js
@@ -2,3 +2,15 @@
 
 import '../components/navbar.js';
 import './homePage.html';
+
+
+import '../components/lightPostSensorForm.js';
+
+Template.homePage.events({
+
+  'click #sumbitSensorState': function(event) {
+     event.preventDefault();
+     
+  }
+
+});
